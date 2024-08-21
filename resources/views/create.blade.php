f5a1e571c36f26397354232ca84a1edaba0d8093
Lucas Martins Menezes <lucasmartinsmenezes@gmail.com>
2024-08-21 14:25:49 -0300

    reusing blade code - subviews

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -1,41 +1,5 @@
 @extends('layouts.app')
 
-@section('title', 'Create Task')
-
-@section('styles')
-    <style>
-        .error-message {
-            color: red;
-            font-size: 0.88rem;
-        }
-        .success-message {
-            color: green;
-            font-size: 0.88rem;
-        }
-    </style>
-@endsection
-
 @section('content')
-    <form method="POST" action="{{ route('tasks.store') }}">
-        @csrf
-        <div>
-            <label for="title">Title</label>
-            <input type="text" name="title" id="title" value="{{ old('title') }}">
-            @error('title')<p class="error-message">{{ $message }}</p>@enderror
-        </div>
-        <div>
-            <label for="title">Description</label>
-            <textarea name="description" id="description" rows="5">{{ old('description') }}</textarea>
-            @error('description')<p class="error-message">{{ $message }}</p>@enderror
-        </div>
-        <div>
-            <label for="title">Long Description</label>
-            <textarea name="long_description" id="long_description" rows="10">{{ old('long_description') }}</textarea>
-            @error('long_description')<p class="error-message">{{ $message }}</p>@enderror
-        </div>
-        <div>
-            <button type="submit">Save</button>
-            <button><a href="/tasks">Cancel</a></button>
-        </div>
-    </form>
+    @include('form')
 @endsection>
--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -1,42 +1,5 @@
 @extends('layouts.app')
 
-@section('title', 'Edit Task')
-
-@section('styles')
-    <style>
-        .error-message {
-            color: red;
-            font-size: 0.88rem;
-        }
-        .success-message {
-            color: green;
-            font-size: 0.88rem;
-        }
-    </style>
-@endsection
-
 @section('content')
-       <form method="POST" action="{{ route('tasks.update', ['task' => $task->id]) }}">
-        @csrf
-        @method('PUT')
-        <div>
-            <label for="title">Title</label>
-            <input type="text" name="title" id="title" value="{{$task->title}}">
-            @error('title')<p class="error-message">{{ $message }}</p>@enderror
-        </div>
-        <div>
-            <label for="title">Description</label>
-            <textarea name="description" id="description" rows="5">{{$task->description}}</textarea>
-            @error('description')<p class="error-message">{{ $message }}</p>@enderror
-        </div>
-        <div>
-            <label for="title">Long Description</label>
-            <textarea name="long_description" id="long_description" rows="10">{{$task->long_description}}</textarea>
-            @error('long_description')<p class="error-message">{{ $message }}</p>@enderror
-        </div>
-        <div>
-            <button type="submit">Save</button>
-            <button><a style="text-decoration: none; color:black"href="/tasks/{{ $task->id }}">Cancel</a></button>
-        </div>
-    </form>
+    @include('form', ['task' => $task])
 @endsection>
