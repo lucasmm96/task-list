b371770be20461f0e6e90694b290277071250cb2
Lucas Martins Menezes <lucasmartinsmenezes@gmail.com>
2024-09-10 16:30:02 -0300

    styling flash message & using Alpine.js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,6 +2,8 @@
 <head>
     <title>Laravel 10 Task List App</title>
     <script src="https://cdn.tailwindcss.com"></script>
+    <script src="//unpkg.com/alpinejs" defer></script>
+
 
     {{-- blade-formatter-disable --}}
     <style type="text/tailwindcss">
@@ -21,7 +23,6 @@
         .error {
             @apply text-red-500 text-sm font-bold
         }
-        /*   */
     </style>
     {{-- blade-formatter-enable --}}
 
@@ -29,9 +30,16 @@
 </head>
 <body class="container mx-auto mt-10 mb-10 max-w-xl">
     <h1 class="text-2xl mb-4">@yield('title')</h1>
-    <div>
+    <div x-data="{ flash: true }">
         @if (session()->has('success'))
-            <div>{{ session('success') }}</div>
+            <div x-show="flash" class="relative mb-10 rounded border border-green-400 bg-green-100 px-4 py-3 text-lg text-green-700" role="alert">
+                <strong class="font-bold">Success!</strong>
+                <div>{{ session('success') }}</div>
+                {{-- <div>This is a flash message</div> --}}
+                <span class="absolute top-0 right-0 bottom-0 px-4 py-3">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" @click="flash = false" stroke="currentColor" class="h-6 w-6 cursor-pointer"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12"/></svg>
+                </span>
+            </div>
         @endif
     </div>
     <div>
